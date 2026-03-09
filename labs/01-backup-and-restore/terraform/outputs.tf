ed940ba2056a0f0e151387b61a71af7c05f51a4d
partha-cloud-architect <108447712+parthapatnaik@users.noreply.github.com>
2026-03-09 23:23:04 +0530

    Add outputs for ALB DNS, application URL, and backups

--- labs/01-backup-and-restore/terraform/outputs.tf
+++ labs/01-backup-and-restore/terraform/outputs.tf
@@ -1,9 +1,31 @@
-output "primary_instance_id" {
-  description = "EC2 instance ID in Mumbai"
-  value       = aws_instance.primary_ec2.id
+output "alb_dns_name" {
+  description = "ALB DNS name"
+  value       = aws_lb.app.dns_name
 }
 
-output "primary_public_ip" {
-  description = "Public IP of EC2 instance in Mumbai"
-  value       = aws_instance.primary_ec2.public_ip
+output "application_url" {
+  description = "Application URL"
+  value = var.create_route53_record
+    ? "http://${var.record_name}.${var.domain_name}"
+    : "http://${aws_lb.app.dns_name}"
+}
+
+output "backup_bucket_name" {
+  description = "S3 backup bucket name"
+  value       = aws_s3_bucket.backup.bucket
+}
+
+output "rds_endpoint" {
+  description = "RDS endpoint"
+  value       = aws_db_instance.app.address
+}
+
+output "backup_vault_mumbai" {
+  description = "AWS Backup vault in Mumbai"
+  value       = aws_backup_vault.mumbai.name
+}
+
+output "backup_vault_hyderabad" {
+  description = "AWS Backup vault in Hyderabad"
+  value       = aws_backup_vault.hyderabad.name
 }
